1829443410fe98f9481cd91f6d5fe3659332af54
AngeKri <aquaraangel@gmail.com>
2024-11-25 12:56:06 +0100

    Update CharacterDetail.vue

--- src/components/CharacterDetail.vue
+++ src/components/CharacterDetail.vue
@@ -11,6 +11,10 @@
       <p>{{ character.age }}</p>
       <p>{{ character.height }}</p>
       <p>{{ character.MBTI }}</p>
+      <div>
+          <p></p>
+          <div class="hex" :style="{backgroundColor: character.colors.color1.display}"></div>
+      </div>
     </div>
     <!-- <div v-else class="characterDetail">
       <p>Select a character to see details.</p>
